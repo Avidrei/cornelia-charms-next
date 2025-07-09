6ad5433dd42c1b8f5dd4f10f2d97c89c653b67a7
Avidrei <drcauba@gmail.com>
2025-07-09 10:41:40 +0800

    Fixed backend and frontend connection

--- app/login/page.js
+++ app/login/page.js
@@ -29,9 +29,9 @@ export default function Login() {
         <div className="relative flex flex-col items-center justify-center max-w-md ">
 
           {/* Background Circle */}
-          <div className="absolute lg:-top-[140%] lg:right-[70%] -top-[130%] right-[60%] min-[500px]:-top-[140%]  min-[500px]:right-[100%] xl:-top-[160%] xl:right-[110%] rounded-full w-[450px] h-[450px]  bg-gradient-to-b from-cornelia-softpink to-cornelia-softwhitemix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+          <div className="absolute lg:-top-[140%] lg:right-[70%] -top-[130%] right-[60%] min-[500px]:-top-[140%]  min-[500px]:right-[100%] xl:-top-[130%] xl:right-[110%] rounded-full w-[450px] h-[450px]  bg-gradient-to-b from-cornelia-softpink to-cornelia-softwhitemix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
           <div style={{ animationDelay: "4s" }} className="absolute -top-[20%] -right-[5%] min-[500px]:-right-[30%] min-[500px]:-top-[30%] w-40 h-40 rounded-full bg-gradient-to-bl from-cornelia-darkpink to-cornelia-softwhite sm:w-54 sm:h-54 md:w-60 md:h-60 lg:w-68 lg:h-68 xl:w-80 xl:h-80 mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
-          <div style={{ animationDelay: "2s" }} className="absolute top-[135%] -right-[50%] min-[500px]:top-[160%] min-[500px]:-right-[70%] lg:-right-[80%] lg:top-[160%] xl:-right-[110%] xl:top-[160%] w-[450px] h-[450px] rounded-full bg-gradient-to-bl from-cornelia-darkpink to-cornelia-softwhite  mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+          <div style={{ animationDelay: "2s" }} className="absolute top-[135%] -right-[50%] min-[500px]:top-[160%] min-[500px]:-right-[70%] lg:-right-[80%] lg:top-[160%] xl:-right-[110%] xl:top-[130%] w-[450px] h-[450px] rounded-full bg-gradient-to-bl from-cornelia-darkpink to-cornelia-softwhite  mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
 
           {/* Login Form Header */}
           <div className="flex flex-col items-center justify-center leading-none text-cornelia-jetblack poppins">
@@ -47,13 +47,13 @@ export default function Login() {
               <input
                 type="email"
                 placeholder="Enter your email"
-                className="w-full p-3 xl:p-4 mb-5 border text-[0.85rem] lg:text-[1rem] xl:text-[1.1rem] border-gray-300 rounded-lg poppins focus:outline-none focus:ring-2 focus:ring-cornelia-darkpink"
+                className="w-full p-3 xl:p-4 mt-2 mb-5 border text-[0.85rem] lg:text-[1rem] xl:text-[1.1rem] border-gray-300 rounded-lg poppins focus:outline-none focus:ring-2 focus:ring-cornelia-darkpink"
               />
               <label className="text-[1.1rem] lg:text-[1.2rem] xl:text-[1.3rem] mb-2 font-medium poppins text-cornelia-jetblack">Password</label>
               <input
                 type="password"
                 placeholder="Enter your password"
-                className="w-full p-3 xl:p-4 mb-5 border text-[0.85rem] lg:text-[1rem] xl:text-[1.1rem] border-gray-300 rounded-lg poppins focus:outline-none focus:ring-2 focus:ring-cornelia-darkpink"
+                className="w-full p-3 xl:p-4 mt-2 mb-5 border text-[0.85rem] lg:text-[1rem] xl:text-[1.1rem] border-gray-300 rounded-lg poppins focus:outline-none focus:ring-2 focus:ring-cornelia-darkpink"
               />
 
               {/* Remember Me and Forgot Password */}
@@ -67,7 +67,7 @@ export default function Login() {
                 </a>
               </div>
               <button
-                type="submit" className="w-full p-2 font-semibold text-white transition-colors duration-300 rounded-lg text-md xl:text-lg lg:p-3 bg-cornelia-softpink poppins hover:bg-cornelia-darkpink">Log In</button>
+                type="submit" className="w-full p-2 font-semibold transition-colors duration-300 rounded-lg text-cornelia-softwhite text-md xl:text-lg lg:p-3 bg-cornelia-softpink poppins hover:bg-cornelia-darkpink">Log In</button>
             </div>
           </form>
         </div>
--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ import messageRoute from './routes/messageRoute.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://corneliacharms.vercel.app'], // frontend dev + prod
+  credentials: true
+}));
+
 app.use(express.json()); 
 app.use('/api/upload', uploadRoute);
 app.use('/api/reviews', reviewRoute);
